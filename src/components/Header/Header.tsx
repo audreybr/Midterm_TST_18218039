cb97d8ae7073501e9467783fbf55e52dccb97162
audreybr <audreybetsy1@gmail.com>
2020-10-20 01:51:47 +0700

    replace color

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ const Header = () : ReactElement => {
   return (
     <div className={styles.container}>
       <div className={styles.title}>
-        UTS Teknologi Sistem Terintegrasi II3160
+        UTS TST II3160
       </div>
       <div className={styles.title}>
         Audrey Betsy
